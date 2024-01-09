761e5aed2eb81f5abc9af9fd60bfeb805b59a142
muthuri-dev <muthurikennedy082@gmail.com>
2024-01-10 02:22:29 +0300

    Blocknote editor and convert the value to markdown

--- components/home/Footer.tsx
+++ components/home/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="border-t">
+    <footer className="border-t absolute w-full">
       <div className="mx-auto max-w-5xl space-y-5 px-3 py-5">
         <div className="flex flex-col justify-between gap-3 sm:flex-row sm:items-center">
           <div className="space-y-2">
