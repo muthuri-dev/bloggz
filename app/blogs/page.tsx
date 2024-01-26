6fa6993d617dd4f5e0b0907fe81d2c2446ccccd4
muthuri-dev <muthurikennedy082@gmail.com>
2024-01-26 09:06:29 +0300

    chores:Protect Blogs page

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -6,9 +6,12 @@ import Link from "next/link";
 import { useQuery } from "@apollo/client";
 import { GET_ALL_BLOGS } from "@/graphql/queries";
 import { IBlogs } from "@/types";
+import useSessionStore from "@/store/useSessionStore";
 
 export default function Blogs() {
   const { data, loading, error } = useQuery(GET_ALL_BLOGS);
+  const sessionStore = useSessionStore();
+  const { session } = sessionStore;
   if (error)
     return (
       <p className="flex justify-center items-center h-[80%] animate-ping ">
@@ -16,45 +19,51 @@ export default function Blogs() {
       </p>
     );
   const blogs: IBlogs[] = data?.blogs;
-  return (
-    <div>
-      <div className="grid grid-cols-6">
-        <div className="col-span-6 md:col-span-4">
-          <div>
-            <ul className="flex flex-wrap border-b">
-              {CategoryNav.map((item) => (
-                <div key={item.id}>
-                  <li className="mr-2">
-                    <Link
-                      href={item.url}
-                      aria-current="page"
-                      className="inline-block rounded-2xl font-thin text-slate-950 rounded-t-lg py-4 px-4 text-sm text-center"
-                    >
-                      {item.value}
-                    </Link>
-                  </li>
+  if (session) {
+    return (
+      <div>
+        <div className="grid grid-cols-6">
+          <div className="col-span-6 md:col-span-4">
+            <div>
+              <ul className="flex flex-wrap border-b">
+                {CategoryNav.map((item) => (
+                  <div key={item.id}>
+                    <li className="mr-2">
+                      <Link
+                        href={item.url}
+                        aria-current="page"
+                        className="inline-block rounded-2xl font-thin text-slate-950 rounded-t-lg py-4 px-4 text-sm text-center"
+                      >
+                        {item.value}
+                      </Link>
+                    </li>
+                  </div>
+                ))}
+              </ul>
+            </div>
+            <div>
+              {loading ? (
+                <div className="h-screen flex justify-center items-center animate-bounce">
+                  Loading....
                 </div>
-              ))}
-            </ul>
+              ) : (
+                blogs.map((blog) => (
+                  <div key={blog.id}>
+                    <Blog blog={blog} />
+                  </div>
+                ))
+              )}
+            </div>
           </div>
-          <div>
-            {loading ? (
-              <div className="h-screen flex justify-center items-center animate-bounce">
-                Loading....
-              </div>
-            ) : (
-              blogs.map((blog) => (
-                <div key={blog.id}>
-                  <Blog blog={blog} />
-                </div>
-              ))
-            )}
+          <div className="col-span-2 border border-l border-gray-200 font-thin hidden md:flex ">
+            Trending and adverts
           </div>
-        </div>
-        <div className="col-span-2 border border-l border-gray-200 font-thin hidden md:flex ">
-          Trending and adverts
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="flex items-center justify-center min-h-24">Login</div>
+    );
+  }
 }
